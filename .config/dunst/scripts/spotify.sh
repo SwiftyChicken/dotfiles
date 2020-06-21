147964dffd8c7ae4327500d370b8944cb43844bc
SwiftyChicken <erwema25@gmail.com>
2020-06-22 01:50:19 +0200

    spt.jpeg is now located in /tmp/ folder. (better placement)

--- .config/dunst/scripts/spotify.sh
+++ .config/dunst/scripts/spotify.sh
@@ -20,9 +20,9 @@ playerctl -p spotify metadata --format "{{ artist }}\n{{ title }}\n{{mpris:artUr
 artist=$(sed '1q;d' $temp| sed -e "s/[,[(]....*[])]*//g"| sed "s/ *$//"| sed "s/^\(.\{28\}[^ ]*\)\(.*\)$/\1[\2]/"| sed "s/\[ *\]$//"| sed "s/\[.*\]$/.../")
 title=$(sed '2q;d' $temp| sed -e "s/[-,[(]....*[])]*//g"| sed "s/ *$//"| sed "s/^\(.\{20\}[^ ]*\)\(.*\)$/\1[\2]/"| sed "s/\[ *\]$//"| sed "s/\[.*\]$/.../" )
 
-curl "$(sed '3q;d' $temp| sed "s/open\.spotify\.com/i.scdn.co/")" > $HOME/.config/dunst/scripts/spt.jpeg \
-	|| cp $HOME/.config/dunst/scripts/error.png $HOME/.config/dunst/scripts/spt.jpeg \
-	&& dunstify -r "991361" -i $HOME/.config/dunst/scripts/spt.jpeg "$title" "<span foreground='#1DB954' font_desc='Ubuntu Mono 32'>󰓇<span font_desc='Ubuntu Mono 14'> </span><b>Spotify</b> </span><span font_desc='Ubuntu Mono 4'>\n \n</span><span font_desc='Ubuntu Mono 22'><b>$artist</b></span>"
+curl "$(sed '3q;d' $temp| sed "s/open\.spotify\.com/i.scdn.co/")" > /tmp/spt.jpeg \
+	|| cp $HOME/.config/dunst/scripts/error.png /tmp/spt.jpeg \
+	&& dunstify -r "991361" -i /tmp/spt.jpeg "$title" "<span foreground='#1DB954' font_desc='Ubuntu Mono 32'>󰓇<span font_desc='Ubuntu Mono 14'> </span><b>Spotify</b> </span><span font_desc='Ubuntu Mono 4'>\n \n</span><span font_desc='Ubuntu Mono 22'><b>$artist</b></span>"
 
 rm $temp
 
