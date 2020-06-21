67f3c3bd1728e922958b087f2d309846a021edfa
SwiftyChicken <erwema25@gmail.com>
2020-06-22 01:20:30 +0200

    Zathura annotation message added and some minor changes

--- .config/dunst/scripts/spotify.sh
+++ .config/dunst/scripts/spotify.sh
@@ -18,11 +18,11 @@ temp=$(mktemp)
 playerctl -p spotify metadata --format "{{ artist }}\n{{ title }}\n{{mpris:artUrl}}"|xargs -0I{} printf "{}" > $temp
 
 artist=$(sed '1q;d' $temp| sed -e "s/[,[(]....*[])]*//g"| sed "s/ *$//"| sed "s/^\(.\{28\}[^ ]*\)\(.*\)$/\1[\2]/"| sed "s/\[ *\]$//"| sed "s/\[.*\]$/.../")
-title=$(sed '2q;d' $temp| sed -e "s/[-,[(]....*[])]*//g"| sed -e "s/[-,[(]....*[])]*//g" | sed "s/ *$//"| sed "s/^\(.\{20\}[^ ]*\)\(.*\)$/\1[\2]/"| sed "s/\[ *\]$//"| sed "s/\[.*\]$/.../" )
+title=$(sed '2q;d' $temp| sed -e "s/[-,[(]....*[])]*//g"| sed "s/ *$//"| sed "s/^\(.\{20\}[^ ]*\)\(.*\)$/\1[\2]/"| sed "s/\[ *\]$//"| sed "s/\[.*\]$/.../" )
 
 curl "$(sed '3q;d' $temp| sed "s/open\.spotify\.com/i.scdn.co/")" > $HOME/.config/dunst/scripts/spt.jpeg \
 	|| cp $HOME/.config/dunst/scripts/error.png $HOME/.config/dunst/scripts/spt.jpeg \
-	&& dunstify -r "991361" -i $HOME/.config/dunst/scripts/spt.jpeg "$title" "<span foreground='#1DB954' font_desc='Ubuntu Mono 32'>󰓇<span font_desc='Ubuntu Mono 14'> </span><b>Spotify</b> </span><span font_desc='Ubuntu Mono 4'>\n \n</span><span font_desc='Ubuntu Mono 22'><b>$artist</b></span>" -h string:bgcolor:#191414
+	&& dunstify -r "991361" -i $HOME/.config/dunst/scripts/spt.jpeg "$title" "<span foreground='#1DB954' font_desc='Ubuntu Mono 32'>󰓇<span font_desc='Ubuntu Mono 14'> </span><b>Spotify</b> </span><span font_desc='Ubuntu Mono 4'>\n \n</span><span font_desc='Ubuntu Mono 22'><b>$artist</b></span>"
 
-rm $temp:w
-k
+rm $temp
+
