b3febb8fae65e9c3e55c06b0642a8737ddfac713
SwiftyChicken <erwema25@gmail.com>
2020-07-09 13:01:33 +0200

    Fix: Sometimes didn't want to hide.

--- .config/polybar/scripts/volume.sh
+++ .config/polybar/scripts/volume.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+
+# Replace if already running
+running=$(pidof -x $0)
+
+while [ "$(echo $running| wc -w)" -gt "1" ]; do
+	oldprocess=$(echo $running| rev| cut -d' ' -f1| rev)
+	kill $oldprocess
+	running=$(pidof -x $0)
+done
+
 polybar-msg cmd show
 xdo raise -a "polybar-level_eDP1"
 
-sleep 1
-
-[ "$(pidof -x $0 | wc -w)" -le "2" ] \
-	&& polybar-msg cmd hide
+sleep 0.8
+polybar-msg cmd hide
 
