cb5fdcecb775af60f92a35732822895c1812b663
SwiftyChicken <erwema25@gmail.com>
2020-07-14 21:31:27 +0200

    Forgot to edit the source.

--- .local/bin/bluebook.sh
+++ .local/bin/bluebook.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 ## ORIGINAL SOURCE: 
-# https://github.com/konapun/dotfiles/blob/master/zsh/custom/greet.zsh
+# https://github.com/bliau03/dotfiles/blob/master/.local/bin/bluebook.sh
 
 BLUE='\033[2;36m'
 WHITE='\033[0;37m'
-TITLE1='\033[2;37m'
-TITLE2='\033[9;37m'
+TITLE1='\033[0;37m'
+TITLE2='\033[9;31m'
 RESET=$(tput sgr0)
 
  
-echo -e "${TITLE2}                 ${RESET}[${TITLE1} E   X        M   A   C   H   I   N   A ${RESET}]${TITLE2}                 ${RESET}"
+echo -e "${TITLE2}                    ${RESET}${TITLE1}E   X      M   A   C   H   I   N   A${RESET}${TITLE2}                    ${RESET}"
 echo -e "${WHITE}   ######   #     #     #  ######   ${BLUE}######     #####      #####    #    #${RESET}"
 echo -e "${WHITE}    #    #  #     #     #  #        ${BLUE} #    #   #     #    #     #   #   # ${RESET}"
 echo -e "${WHITE}     #   #  #     #     #  #        ${BLUE}  #   #  #       #  #       #  #  #  ${RESET}"
@@ -20,4 +20,4 @@ echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #  #       #  
 echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #  #       #  #       #  #  #  ${RESET}"
 echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #   #     #    #     #   #   # ${RESET}"
 echo -e "${WHITE}   ######   ####   #####   ######   ${BLUE}######     #####      #####    #    #${RESET}"
-echo -e "${TITLE2}              ${RESET}[${TITLE1} B   E   Y   O   N   D   S   E   A   R   C   H ${RESET}]${TITLE2}             ${RESET}"
+echo -e "${TITLE2}                ${RESET}${TITLE1}B   E   Y   O   N   D   S   E   A   R   C   H${RESET}${TITLE2}               ${RESET}"
