06f620b9fc34b9f9f12957e53d60694a68a93554
SwiftyChicken <erwema25@gmail.com>
2020-07-14 12:13:52 +0200

    Imroved version, I guess...

--- .local/bin/bluebook.sh
+++ .local/bin/bluebook.sh
@@ -3,18 +3,21 @@
 ## ORIGINAL SOURCE: 
 # https://github.com/konapun/dotfiles/blob/master/zsh/custom/greet.zsh
 
-BLUE='\033[0;36m'
+BLUE='\033[2;36m'
 WHITE='\033[0;37m'
-TITLE='\033[2;37m'
+TITLE1='\033[2;37m'
+TITLE2='\033[9;37m'
+RESET=$(tput sgr0)
 
  
-echo -e "${TITLE}                   E   X        M   A   C   H   I   N   A          ${WHITE}"
-echo -e "${WHITE}	#####  #    #    # #####  ${BLUE}#####    ####     ####   #    #${WHITE}"
-echo -e "${WHITE}	 #   # #    #    # #      ${BLUE} #   #  #    #   #    #  #   # ${WHITE}"
-echo -e "${WHITE}	  #  # #    #    # #      ${BLUE}  #  # #      # #      # #  #  ${WHITE}"
-echo -e "${WHITE}	 ####  #    #    # #####  ${BLUE} ####  #      # #      # # #   ${WHITE}"
-echo -e "${WHITE}	#    # #    #    # #      ${BLUE}#    # #      # #      # #  #  ${WHITE}"
-echo -e "${WHITE}	#    # #    #    # #      ${BLUE}#    #  #    #   #    #  #   # ${WHITE}"
-echo -e "${WHITE}	#####  ####  ####  #####  ${BLUE}#####    ####     ####   #    #${WHITE}"
-echo -e "${TITLE}                B   E   Y   O   N   D   S   E   A   R   C   H      ${WHITE}"
-  echo ""
+echo -e "${TITLE2}                 ${RESET}[${TITLE1} E   X        M   A   C   H   I   N   A ${RESET}]${TITLE2}                 ${RESET}"
+echo -e "${WHITE}   ######   #     #     #  ######   ${BLUE}######     #####      #####    #    #${RESET}"
+echo -e "${WHITE}    #    #  #     #     #  #        ${BLUE} #    #   #     #    #     #   #   # ${RESET}"
+echo -e "${WHITE}     #   #  #     #     #  #        ${BLUE}  #   #  #       #  #       #  #  #  ${RESET}"
+echo -e "${WHITE}      #  #  #     #     #  #        ${BLUE}   #  #  #       #  #       #  # #   ${RESET}"
+echo -e "${WHITE}    #####   #     #     #  ######   ${BLUE} #####   #       #  #       #  ##    ${RESET}"
+echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #  #       #  #       #  # #   ${RESET}"
+echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #  #       #  #       #  #  #  ${RESET}"
+echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #   #     #    #     #   #   # ${RESET}"
+echo -e "${WHITE}   ######   ####   #####   ######   ${BLUE}######     #####      #####    #    #${RESET}"
+echo -e "${TITLE2}              ${RESET}[${TITLE1} B   E   Y   O   N   D   S   E   A   R   C   H ${RESET}]${TITLE2}             ${RESET}"
