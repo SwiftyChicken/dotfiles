c6830a074f97245f5b3ea65c128d0c4ebaa95a17
SwiftyChicken <erwema25@gmail.com>
2020-07-14 11:09:51 +0200

    bar to title added

--- .local/bin/hanka.sh
+++ .local/bin/hanka.sh
@@ -6,10 +6,14 @@
 columns=$(tput cols)
 [[ $columns -le 70 ]] && exit 0
 
-RED='\033[0;31m'
+RED='\033[1;31m'
 WHITE='\033[1;37m'
+TITLE1='\033[2;37m'
+TITLE2='\033[7;37m'
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
  
-  echo -e "     ${WHITE}              人  工  知  能  や  ロ  ボ  ッ  ト  エ  学              "
+  echo -e "  ${TITLE2}${TITLE1}${BOLD}              人  工  知  能  や  ロ  ボ  ッ  ト  エ  学                   ${RESET}"
   echo -e "     ${RED}####    ####       ###     ####      #### ####    #####      ###"
   echo -e "     ${RED}####    ####      #####    #####     #### ####   #####      #####"
   echo -e "     ${RED}####    ####     #######   ######    #### #### #####       #######"
@@ -18,6 +22,6 @@ WHITE='\033[1;37m'
   echo -e "     ${RED}####    ####  ####     ######## #### #### ##### #####   ####     ####"
   echo -e "     ${RED}####    #### ####       #######  ######## ####   ##### ####       ####"
   echo -e "     ${RED}####    ########         ######   ####### ####    ########         ####"
-  echo -e "     ${WHITE}       R       O       B       O       T       I       C       S      "
+  echo -e "  ${TITLE2}${TITLE1}${BOLD}       R       O       B       O       T       I       C       S            ${RESET}"
   echo ""
 exit 0
