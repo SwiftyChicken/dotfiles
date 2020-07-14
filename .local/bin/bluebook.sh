91bf4e05998183c7f99b19b54aaad62023ececae
SwiftyChicken <erwema25@gmail.com>
2020-07-14 11:11:37 +0200

    New greeter from bliau03 fork

---
+++ .local/bin/bluebook.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+## ORIGINAL SOURCE: 
+# https://github.com/konapun/dotfiles/blob/master/zsh/custom/greet.zsh
+
+BLUE='\033[0;36m'
+WHITE='\033[0;37m'
+TITLE='\033[2;37m'
+
+ 
+echo -e "${TITLE}                   E   X        M   A   C   H   I   N   A          ${WHITE}"
+echo -e "${WHITE}	#####  #    #    # #####  ${BLUE}#####    ####     ####   #    #${WHITE}"
+echo -e "${WHITE}	 #   # #    #    # #      ${BLUE} #   #  #    #   #    #  #   # ${WHITE}"
+echo -e "${WHITE}	  #  # #    #    # #      ${BLUE}  #  # #      # #      # #  #  ${WHITE}"
+echo -e "${WHITE}	 ####  #    #    # #####  ${BLUE} ####  #      # #      # # #   ${WHITE}"
+echo -e "${WHITE}	#    # #    #    # #      ${BLUE}#    # #      # #      # #  #  ${WHITE}"
+echo -e "${WHITE}	#    # #    #    # #      ${BLUE}#    #  #    #   #    #  #   # ${WHITE}"
+echo -e "${WHITE}	#####  ####  ####  #####  ${BLUE}#####    ####     ####   #    #${WHITE}"
+echo -e "${TITLE}                B   E   Y   O   N   D   S   E   A   R   C   H      ${WHITE}"
+  echo ""
