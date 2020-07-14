9442dee65e10242e49c802e644964e39429bdadd
SwiftyChicken <erwema25@gmail.com>
2020-07-14 21:40:20 +0200

    Forgot to edit souce

--- .local/bin/bluebook.sh
+++ .local/bin/bluebook.sh
@@ -5,12 +5,12 @@
 
 BLUE='\033[2;36m'
 WHITE='\033[0;37m'
-TITLE1='\033[0;37m'
-TITLE2='\033[9;31m'
+TITLE1='\033[2;37m'
+TITLE2='\033[9;37m'
 RESET=$(tput sgr0)
 
  
-echo -e "${TITLE2}                    ${RESET}${TITLE1}E   X      M   A   C   H   I   N   A${RESET}${TITLE2}                    ${RESET}"
+echo -e "${TITLE2}                   ${RESET}[${TITLE1} E   X      M   A   C   H   I   N   A ${RESET}]${TITLE2}                   ${RESET}"
 echo -e "${WHITE}   ######   #     #     #  ######   ${BLUE}######     #####      #####    #    #${RESET}"
 echo -e "${WHITE}    #    #  #     #     #  #        ${BLUE} #    #   #     #    #     #   #   # ${RESET}"
 echo -e "${WHITE}     #   #  #     #     #  #        ${BLUE}  #   #  #       #  #       #  #  #  ${RESET}"
@@ -20,4 +20,4 @@ echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #  #       #  
 echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #  #       #  #       #  #  #  ${RESET}"
 echo -e "${WHITE}   #     #  #     #     #  #        ${BLUE}#     #   #     #    #     #   #   # ${RESET}"
 echo -e "${WHITE}   ######   ####   #####   ######   ${BLUE}######     #####      #####    #    #${RESET}"
-echo -e "${TITLE2}                ${RESET}${TITLE1}B   E   Y   O   N   D   S   E   A   R   C   H${RESET}${TITLE2}               ${RESET}"
+echo -e "${TITLE2}               ${RESET}[${TITLE1} B   E   Y   O   N   D   S   E   A   R   C   H ${RESET}]${TITLE2}              ${RESET}"
