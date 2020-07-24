d1221c5fefa743c1616f82bf93225e4155a93215
SwiftyChicken <erwema25@gmail.com>
2020-07-24 23:29:06 +0200

    Revert to old version

--- .local/bin/hanka.sh
+++ .local/bin/hanka.sh
@@ -8,12 +8,11 @@ columns=$(tput cols)
 
 RED='\033[1;31m'
 WHITE='\033[1;37m'
-TITLE1='\033[2;37m'
-TITLE2='\033[7;37m'
+TITLE='\033[0;37m'
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
  
-  echo -e "  ${TITLE2}${TITLE1}${BOLD}              人  工  知  能  や  ロ  ボ  ッ  ト  エ  学                   ${RESET}"
+  echo -e "  ${TITLE}${BOLD}              人  工  知  能  や  ロ  ボ  ッ  ト  エ  学                   ${RESET}"
   echo -e "     ${RED}####    ####       ###     ####      #### ####    #####      ###"
   echo -e "     ${RED}####    ####      #####    #####     #### ####   #####      #####"
   echo -e "     ${RED}####    ####     #######   ######    #### #### #####       #######"
@@ -22,6 +21,6 @@ RESET=$(tput sgr0)
   echo -e "     ${RED}####    ####  ####     ######## #### #### ##### #####   ####     ####"
   echo -e "     ${RED}####    #### ####       #######  ######## ####   ##### ####       ####"
   echo -e "     ${RED}####    ########         ######   ####### ####    ########         ####"
-  echo -e "  ${TITLE2}${TITLE1}${BOLD}       R       O       B       O       T       I       C       S            ${RESET}"
+  echo -e "  ${TITLE}${BOLD}       R       O       B       O       T       I       C       S            ${RESET}"
   echo ""
 exit 0
