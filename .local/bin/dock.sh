9c28f6ce435474c246bdafd28d90a06864036a30
SwiftyChicken <erwema25@gmail.com>
2021-11-24 09:39:30 +0100

    Polyar: Fix polybar to open at start up

--- .local/bin/dock.sh
+++ .local/bin/dock.sh
@@ -3,16 +3,13 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Wait until the processes have been shut down and bspwm is started
+while pgrep -u $UID -x polybar >/dev/null || ! pgrep -u $UID -x bspwm >/dev/null; do sleep 1; done
 
 # Wait until the control has started
-polybar level &
 polybar control &
 polybar workspace &
 pgrep spotify && polybar player &
 polybar status &
 polybar power &
 
-while [ -z "$(find /tmp -maxdepth 1 -name "polybar*")" ]; do sleep 1; done
-polybar-msg cmd hide &
