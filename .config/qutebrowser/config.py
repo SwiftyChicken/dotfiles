ab4832e894b7f4b7cd8470da64dd4064ce6b299f
SwiftyChicken <erwema25@gmail.com>
2021-11-30 00:10:32 +0100

    REMOVE: restored f & F bindings

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,19 +1,19 @@
 import dracula.draw
 
-# Load existing settings made via :set
+### Load existing settings made via :set
 config.load_autoconfig()
 
-# Font
-c.fonts.default_family = 'BreezeSans'
-c.fonts.default_size = '13pt'
+### Font
+c.fonts.default_family = 'UbuntuMono Nerd Font'
+c.fonts.default_size = '14pt'
 
-# Improved find
-config.bind('f', 'hint links spawn --detach urlhandler {hint-url}')
-config.bind('F', 'hint links spawn --detach urlhandler -t {hint-url}')
-config.bind('gi', 'spawn --detach urlhandler {url:pretty}')
-config.bind('gI', 'spawn --detach urlhandler -t {url:pretty}')
+### Open Videos in mpv if possible
+# config.bind('f', 'hint links spawn --detach urlhandler {hint-url}')
+# config.bind('F', 'hint links spawn --detach urlhandler -t {hint-url}')
+# config.bind('gi', 'spawn --detach urlhandler {url:pretty}')
+# config.bind('gI', 'spawn --detach urlhandler -t {url:pretty}')
 
-# Load theme
+### Load theme
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
