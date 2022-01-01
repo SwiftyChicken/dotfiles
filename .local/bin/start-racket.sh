dd00e9dd57180a74aceba94933f431332c2f899d
SwiftyChicken <erwema25@gmail.com>
2022-01-02 00:40:32 +0100

    REMOVE: Unnecessary scripts

--- .local/bin/start-racket.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-RLANG="$(grep "^#lang " "$@" |sed "s/^#lang \?//g"|tr -d '\n' )"
-RPATH="$HOME/.local/lib/r7rs/"
-racket -I $RLANG -S $RPATH -if <(sed "/^#lang \?/d" "$@")
--- .local/bin/timer-checkupdates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-file=/tmp/updates
-
-echo "$(checkupdates | wc -l)" > $file
-echo "$(yay -Qum | wc -l)" >> $file
-
-exit 0
