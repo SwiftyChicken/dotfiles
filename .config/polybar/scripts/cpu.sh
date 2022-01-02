7ad15577259a681b7487bf0abe0be73bb7dbe5a8
SwiftyChicken <erwema25@gmail.com>
2022-01-02 18:09:05 +0100

    MOD: replace calc with bc

--- .config/polybar/scripts/cpu.sh
+++ .config/polybar/scripts/cpu.sh
@@ -10,11 +10,12 @@ temperature=$(tail -n1 $file)
 
 useage=$(echo "${useage%%.*}" | sed "s/^\(..\)$/\1  /" | sed "s/^\(.\)$/\1    /" )
 
+
 [[ "$ex" == "false" ]] && echo "%{T1}%{F#dae1ec}$useage%{T3}%{O-19}$temperature%{T-}%{F-}"
 
-useage=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat)| tr -s ' ' | cut -d' ' -f2,4,5| tr '\n' ' '| sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) $/(\1-\4+\2-\5)*100\/(\1-\4+\2-\5+\3-\6)/g"| calc -dp)
+useage=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat)| tr -s ' ' | cut -d' ' -f2,4,5| tr '\n' ' '| sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) $/(\1-\4+\2-\5)*100\/(\1-\4+\2-\5+\3-\6)/g")
 temperature=$(cat /sys/class/thermal/thermal_zone*/temp | tail -n1 | sed 's/...$/°/')
 
-echo "$useage" > $file
+echo "$useage"| bc > $file
 echo "$temperature" >> $file
 [[ "$ex" == "true" ]] && exit 1 || exit 0
