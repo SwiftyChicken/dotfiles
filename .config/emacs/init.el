a7632e58150ad2b0327487ca80c51f1b63aa9dc3
Erneste R. Rwema Rugema <richard@niyonzima.com>
2023-04-17 20:13:59 +0200

    MOD: autoconfig

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -83,7 +83,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons use-package undo-tree evil-collection dashboard command-log-mode)))
+   '(yuck-mode haskell-mode all-the-icons use-package undo-tree evil-collection dashboard command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
